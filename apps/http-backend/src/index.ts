9854cb8ead80bfc763965eb6f4b3d1cb65f64f19
Anijeet Mani <146390261+Anijeet@users.noreply.github.com>
2025-03-27 04:19:14 +0530

    Update index.ts

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -9,7 +9,15 @@ import cors from "cors"
 
 const app=express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: [
+      'http://35.154.89.74:3000', 
+      'http://localhost:3000',
+    
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
 
 app.get('/',(req,res)=>{  
     res.send('Hello World!')
